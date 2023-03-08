b134df8ae483a63a81b7b0526ece06adcbad9cf8
Bigtoxa766 <bigtoxa766@gmail.com>
2023-03-08 23:05:36 +0200

    Update task 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+
 console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery')
+
+const makeGalleryGrid = ({ preview, original, description }) => `
+<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+
+const markupGallery = galleryItems.map(e => makeGalleryGrid(e)).join('');
+galleryEl.insertAdjacentHTML('afterbegin', markupGallery)
+
+new SimpleLightbox('.gallery a',
+  { captionsData: 'alt', captionDelay: 250 })
