8d4c03d9c6f59770b5667db4b69bb8399d918478
Bigtoxa766 <bigtoxa766@gmail.com>
2023-03-11 20:46:03 +0200

    update task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle'
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const LOCAL_STORAGE_KEY = "videoplayer-current-time";
+const currentTime = localStorage.getItem(LOCAL_STORAGE_KEY)
+  ? localStorage.getItem(LOCAL_STORAGE_KEY)
+  : 0;
+
+player.on('timeupdate', throttle(storageFunc, 1000));
+
+function storageFunc(e) {
+  localStorage.setItem(LOCAL_STORAGE_KEY, e.seconds)
+}
+
+player.setCurrentTime(currentTime)
+
+
+
+    
