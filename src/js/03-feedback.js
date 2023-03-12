39c8fc637c043013087c71c53c2748da3140800f
Bigtoxa766 <bigtoxa766@gmail.com>
2023-03-12 02:25:37 +0200

    update task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import throttle from 'lodash.throttle'
 
+const formEl = document.querySelector('.feedback-form');
+const FORM_LOCAL_STORAGE_KEY = "feedback-form-state";
+const formData = {};
+
+populateTextValue()
+
+formEl.addEventListener('submit', handleFofmSubmit)
+formEl.addEventListener('input', throttle(getFormData, 500));
+
+function getFormData(e) {
+  formData[e.target.name] = e.target.value;
+
+  localStorage.setItem(FORM_LOCAL_STORAGE_KEY, JSON.stringify(formData));
+}
+
+function handleFofmSubmit(e) {
+  e.preventDefault();
+  e.target.reset();
+  localStorage.removeItem(FORM_LOCAL_STORAGE_KEY)
+}
+
+function populateTextValue() {
+  let savedMessage = localStorage.getItem(FORM_LOCAL_STORAGE_KEY);
+
+  if (savedMessage) {
+    // console.log(savedMessage);
+    // Object.values(formData) = savedMessage;
+  }
+}
