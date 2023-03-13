350267ce2d9d7b0f091aceb2afceac500c8437b4
Bigtoxa766 <bigtoxa766@gmail.com>
2023-03-13 19:01:22 +0200

    update task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,22 +2,33 @@ import throttle from 'lodash.throttle'
 
 const formEl = document.querySelector('.feedback-form');
 const FORM_LOCAL_STORAGE_KEY = "feedback-form-state";
-const formData = {};
+const formData = {
+  email: '',
+  message: '',
+};
 
-formEl.addEventListener('submit', handleFofmSubmit)
+formEl.addEventListener('submit', handleFofmSubmit);
 formEl.addEventListener('input', throttle(getFormData, 500));
 
 function getFormData(e) {
   formData[e.target.name] = e.target.value;
 
-  localStorage.setItem(FORM_LOCAL_STORAGE_KEY, JSON.stringify(formData));
+  localStorage.setItem(FORM_LOCAL_STORAGE_KEY,
+    JSON.stringify({
+      ...formData, email: formEl.email.value,
+      message: formEl.message.value
+    }));
 }
 
 function handleFofmSubmit(e) {
   e.preventDefault();
-  e.target.reset();
+  if (formEl.email.value !== '' && formEl.message.value !== '') {
+    e.target.reset();
   localStorage.removeItem(FORM_LOCAL_STORAGE_KEY)
   console.log(formData)
+  } else {
+    alert('All fields must be filled');
+  }
 }
 
 function populateTextValue() {
@@ -29,6 +40,5 @@ function populateTextValue() {
   }
   console.log(formEl.email.value)
 }
-
 
 populateTextValue()
